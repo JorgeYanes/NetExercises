c66af88e469ed1b1318404bb4d2f90e7a120f0db
Jorge Yanes <jorgeyanesm@gmail.com>
2021-03-26 11:09:41 -0400

    primera version ejercicio1

--- Ejercicio1/Ejercicio1/Program.cs
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -6,7 +6,17 @@ namespace Ejercicio1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Returns the smallest positive integer that does not occur in array");
+            int small = 0;
+            int[] numbers = { 1, 3, 5, 7, 9 };
+            small = smallest(numbers);
+            Console.WriteLine("smallest is:{0}",small);
+            Console.ReadLine();
+        }
+
+        public static int smallest(int[] numbers) 
+        {
+            return numbers[0];
         }
     }
 }
